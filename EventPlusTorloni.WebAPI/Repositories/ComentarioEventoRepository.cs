c4c50ebf48df400b429701447c9265227459eab0
marcaumdev <marcaumdev@gmail.com>
2026-03-24 08:57:50 -0300

    comentario repository

---
+++ EventPlusTorloni.WebAPI/Repositories/ComentarioEventoRepository.cs
@@ -0,0 +1,86 @@
+﻿using EventPlusTorloni.WebAPI.BdContextEvent;
+using EventPlusTorloni.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlusTorloni.WebAPI.Repositories;
+
+public class ComentarioEventoRepository
+{
+    private readonly EventContext _context;
+
+    public ComentarioEventoRepository(EventContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    /// Método para buscar um comentário específico
+    /// </summary>
+    /// <param name="idUsuario">idUsuario</param>
+    /// <param name="IdEvento">idEvento</param>
+    /// <returns>Comentário Buscado</returns>
+    public ComentarioEvento BuscarPorIdUsuario(Guid idUsuario, Guid IdEvento)
+    {
+        try
+        {
+            // Usamos Include para trazer os dados relacionados sem precisar "mapear na mão"
+            return _context.ComentarioEventos
+                .Include(c => c.IdUsuarioNavigation)
+                .Include(c => c.IdEventoNavigation)
+                .FirstOrDefault(c => c.IdUsuario == idUsuario && c.IdEvento == IdEvento)!;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Método para cadastrar um comentário
+    /// </summary>
+    /// <param name="comentarioEvento">Comentário a ser cadastrado</param>
+    public void Cadastrar(ComentarioEvento comentarioEvento)
+    {
+        _context.ComentarioEventos.Add(comentarioEvento);
+        _context.SaveChanges();
+    }
+
+    /// <summary>
+    /// Método para deletar um comentário
+    /// </summary>
+    /// <param name="IdComentarioEvento">Id do comentário a ser excluído</param>
+    public void Deletar(Guid IdComentarioEvento)
+    {
+        var comentarioBuscado = _context.ComentarioEventos.Find(IdComentarioEvento);
+
+        if (comentarioBuscado != null)
+        {
+            _context.ComentarioEventos.Remove(comentarioBuscado);
+            _context.SaveChanges();
+        }
+    }
+
+    /// <summary>
+    /// Método para listar os comentários
+    /// </summary>
+    /// <returns>Lista dos comentários</returns>
+    /// <param name="IdEvento">Id do evento a ter os comentários buscados</param>
+    public List<ComentarioEvento> Listar(Guid IdEvento)
+    {
+        return _context.ComentarioEventos
+        .Include(c => c.IdUsuarioNavigation)
+        .Include(c => c.IdEventoNavigation)
+        .Where(c => c.IdEvento == IdEvento)
+        .ToList();
+    }
+
+    public List<ComentarioEvento> ListarSomenteExibe(Guid IdEvento)
+    {
+        return _context.ComentarioEventos
+            .Include(c => c.IdUsuarioNavigation)
+            .Include(c => c.IdEventoNavigation)
+            .Where(c => c.Exibe == true && c.IdEvento == IdEvento)
+            .ToList();
+    }
+
+}
--- EventPlusTorloni.WebAPI/obj/Debug/net10.0/EventPlusTorloni.WebAPI.AssemblyInfo.cs
+++ EventPlusTorloni.WebAPI/obj/Debug/net10.0/EventPlusTorloni.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventPlusTorloni.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50f63ba0ee90e4fbe2899ed9b7456f744b194544")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventPlusTorloni.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventPlusTorloni.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
